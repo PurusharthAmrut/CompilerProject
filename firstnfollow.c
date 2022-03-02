a7a76438b29636bf781d87b0903cc2f7501ac49a
KushMehta1511 <f20180956@pilani.bits-pilani.ac.in>
2022-03-02 17:50:05 +0530

    changes in firstnfollow

--- firstnfollow.c
+++ firstnfollow.c
@@ -1,5 +1,4 @@
 #include "parserDef.h"
-#include <stdio.h>
 
 #define EXCLUDE_EPS -288230376151711745L
 #define INCLUDE_EPS 288230376151711744L
@@ -7,6 +6,7 @@
 void first(Grammar g, rhsCharNode rcn, long int* firstBitString)
 
 void computeFirst(Grammar g, nonTerminal nt, long int* firstBitString) {
+    //this function assumes
     lhsChar lc = g[nt];
     for (int i=0; i<lc->numRules; i++) {
         first(lc->heads[i], firstBitString);
@@ -14,6 +14,8 @@ void computeFirst(Grammar g, nonTerminal nt, long int* firstBitString) {
 }
 
 void first(Grammar g, rhsCharNode rcn, long int* firstBitString) {
+    // this funcyion assumes that the caller has already checked whether
+    // the first set for nt has been previosly computed before calling this function
     if (rcn->tag==0) { //symbol is a terminal
         *firstBitString = *firstBitString | 1<<(rcn->s.t);
     } else { //symbol is a non-terminal => further digging needed
@@ -35,4 +37,46 @@ void first(Grammar g, rhsCharNode rcn, long int* firstBitString) {
         }
     }
     return;
+}
+
+void computeFollow(Grammar g, nonTerminal nt) {
+    // the function assumes that the caller has first checked if the follow set
+    // of nt has already been computed
+    rhsCharNode rcn, nextrcn;
+    long int firstOfNext;
+    for (int i=0; i<NO_OF_NONTERMINALS; i++) {
+        for (int j=0; j<g[i].numRules; j++) {
+            rcn = g[i].heads[j];
+            while (rcn!=NULL) {
+                if (rcn->tag==1 && rcn->s.nt==nt) {
+                    if (rcn->next !=NULL) {
+                        nextrcn = rcn->next;
+                        first(g, nextrcn, &firstOfNext);
+                        if ((firstOfNext>>eps) & 1 == 1) {
+                            // first set of subsequent string contains eps
+                            firstOfNext = firstOfNext & EXCLUDE_EPS;
+                            g[nt].follow = g[nt].follow | firstOfNext;
+                            if (i!=nt) // if the nonTerminal on LHS is different than nt
+                            {
+                                if (g[i].follow == 0)
+                                    computeFollow(g, i);
+                                g[nt].follow = g[nt].follow | g[i].follow;                             
+                            }
+                        } else {
+                            g[nt].follow = g[nt].follow | firstOfNext;
+                        }
+                    } else {
+                        //This is the last character in the RHS of the production
+                        if (i!=nt) // if the nonTerminal on LHS is different than nt
+                        {
+                            if (g[i].follow == 0)
+                                computeFollow(g, i);
+                            g[nt].follow = g[nt].follow | g[i].follow;                             
+                        }
+                    }
+                }
+                rcn = rcn->next;
+            }
+        }
+    }
 }
