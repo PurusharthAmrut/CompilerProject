34a1d404aaed4e07b7ffc0710bf700ebc0daa1c5
KushMehta1511 <f20180956@pilani.bits-pilani.ac.in>
2022-03-03 14:57:28 +0530

    parser.h unused functions

--- parser.h
+++ parser.h
@@ -14,19 +14,19 @@
 #include "parserDef.h"
 #include "lexerDef.h"
 
-int* calculateFirst(int production, Grammar g, FirstSet firstSet);
-int* add(int* ans, int* addit);
-void buildFirstSet(Grammar g, FirstSet firstSet);
+// int* calculateFirst(int production, Grammar g, FirstSet firstSet);
+// int* add(int* ans, int* addit);
+// void buildFirstSet(Grammar g, FirstSet firstSet);
 char* getTermString(terminal term);
 char* getNonTermString(nonTerminal nonTerm);
 terminal checkTerminal(char *tokenArr);
 nonTerminal checkNonTerminal(char *tokenArr);
 void printGram(Grammar g);
 void getGram(char *fname, Grammar g);
-void getFollowSets(Grammar g, FollowSet fw, FirstSet fr);
-void findFollow(int NonTerminalNo, int productionNo, Grammar g, FollowSet fw, FirstSet fr,int* flag);
+// void getFollowSets(Grammar g, FollowSet fw, FirstSet fr);
+// void findFollow(int NonTerminalNo, int productionNo, Grammar g, FollowSet fw, FirstSet fr,int* flag);
 void createParseTable(FirstSet firstSet,FollowSet followSet,Grammar g,Table t);
-void add2(int* ans,int* add,int* flag);
+// void add2(int* ans,int* add,int* flag);
 void parseInputSourceCode(FILE* sourceFile,Table t,Grammar g,parseTree root,int* error);
 void printParseTree(parseTree root);
 
